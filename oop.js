95ce72db16bcd00f2470d3b7fbb1eba4441c7e65
disaisoft <disaisoft@gmail.com>
2021-06-01 19:58:51 -0500

    creación de clases

--- oop.js
+++ oop.js
@@ -1,12 +1,19 @@
 class Book {
-
-    title ='1984';
-    author = 'George Orwell';
-    price = '350';
+    constructor(title, author, price){
+        this._title = title;
+        this._author= author;
+        this._price = price;
+    }
 }
 
-
 //Instancia de Book:
-const book1 = new Book();
+const book1 = new Book('1984', 'G.O', 350);
+const book2 = new Book('Frankestain', 'M.S', 200);
 
-console.log( book1.title);
+console.log( book1._title);
+console.log( book1._author);
+console.log(book1._price);
+
+console.log( book2._title);
+console.log( book2._author);
+console.log(book2._price);
